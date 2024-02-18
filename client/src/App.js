80dc96b3b3536ee309754a9a91375493ca56ad21
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2024-02-18 11:17:17 +0200

    be

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+
+import About from './components/About';
+import Articles from './components/Articles';
+import Choosing from './components/Choosing';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+       
+      <Header/>
+      <About/>
+     
+      <Choosing/>
+      <Articles/>
+      <Footer/>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
