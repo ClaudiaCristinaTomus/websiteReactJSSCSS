9a5b2c9aef263208099fb140ab263c064ce97867
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2024-02-18 12:02:03 +0200

    be

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/message", (req, res) => {
-  res.json({ message: "hddddddddddddddddddddddddddddddddddddddddddddddddddddddddello"});
+  res.json({ message: ""});
  
 });
 
